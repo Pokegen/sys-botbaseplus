fca19e1b1c449dbffdfed38a9a2c7423a01e4e38
DevYukine <devyukine@gmx.de>
2020-05-31 21:17:22 +0200

    feat(metadata): add metadata endpoint

--- sys-botbaseplus/source/http.cpp
+++ sys-botbaseplus/source/http.cpp
@@ -29,6 +29,18 @@ void Http::registerRoutes(httplib::Server* svr)
 		setContent(res, Json::toString(d));
 	});
 
+	svr->Get("/metadata", [](const httplib::Request& req, httplib::Response& res) 
+	{
+		std::shared_ptr<Document> d = Json::createDocument();
+
+		Commands::MetaData meta = Commands::getMetaData();
+
+		d->AddMember("titleId", Util::str_fmt("%16lX", meta.titleID), d->GetAllocator());
+		d->AddMember("buildId", Util::str_fmt("%02x%02x%02x%02x%02x%02x%02x%02x\n", meta.buildID[0], meta.buildID[1], meta.buildID[2], meta.buildID[3], meta.buildID[4], meta.buildID[5], meta.buildID[6], meta.buildID[7]), d->GetAllocator());
+
+		setContent(res, Json::toString(d));
+	});
+
 	svr->Post("/", [](const httplib::Request& req, httplib::Response& res)
 	{
 		std::unique_ptr<Document> document = std::make_unique<Document>();
