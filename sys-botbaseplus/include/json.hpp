a78093a288c02da00c7f02f90bdb1d865674818e
DevYukine <devyukine@gmx.de>
2020-05-31 02:52:44 +0200

    feat(rapidjson): move RAPIDJSON_HAS_STDSTRING to header file

--- sys-botbaseplus/include/json.hpp
+++ sys-botbaseplus/include/json.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#define RAPIDJSON_HAS_STDSTRING 1
+
 #include <memory>
 #include "rapidjson/document.h"
 
--- sys-botbaseplus/source/json.cpp
+++ sys-botbaseplus/source/json.cpp
@@ -1,5 +1,3 @@
-#define RAPIDJSON_HAS_STDSTRING 1
-
 #include "json.hpp"
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
