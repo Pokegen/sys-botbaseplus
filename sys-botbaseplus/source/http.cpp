0d86b3f1e6ce54078807edaf24ab54e8913154c1
Yukine <devyukine@gmx.de>
2021-11-28 20:03:28 +0100

    feat: add mode support to poke and peek

--- sys-botbaseplus/source/http.cpp
+++ sys-botbaseplus/source/http.cpp
@@ -14,182 +14,218 @@ void Http::registerRoutes(httplib::Server *svr)
 {
 	svr->Get("/version", [](const httplib::Request &req, httplib::Response &res)
 			 {
-		if (validateAuthentication(req, res) == false)
-			return;
+				 if (validateAuthentication(req, res) == false)
+					 return;
 
-		std::shared_ptr<Document> d = Json::createDocument();
+				 std::shared_ptr<Document> d = Json::createDocument();
 
-		d->AddMember("version", Constants::version, d->GetAllocator());
+				 d->AddMember("version", Constants::version, d->GetAllocator());
 
-		setContent(res, Json::toString(d));
-	});
+				 setContent(res, Json::toString(d));
+			 });
 
 	svr->Get("/healthz", [](const httplib::Request &req, httplib::Response &res)
 			 {
-		if (validateAuthentication(req, res) == false)
-			return;
+				 if (validateAuthentication(req, res) == false)
+					 return;
 
-		std::shared_ptr<Document> d = Json::createDocument();
+				 std::shared_ptr<Document> d = Json::createDocument();
 
-		d->AddMember("status", "UP", d->GetAllocator());
+				 d->AddMember("status", "UP", d->GetAllocator());
 
-		setContent(res, Json::toString(d));
-	});
+				 setContent(res, Json::toString(d));
+			 });
 
 	svr->Get("/metadata", [](const httplib::Request &req, httplib::Response &res)
 			 {
-		if (validateAuthentication(req, res) == false)
-			return;
+				 if (validateAuthentication(req, res) == false)
+					 return;
 
-		std::shared_ptr<Document> d = Json::createDocument();
+				 std::shared_ptr<Document> d = Json::createDocument();
 
-		Commands::MetaData meta = Commands::getMetaData();
+				 Commands::MetaData meta = Commands::getMetaData();
 
-		d->AddMember("titleId", Util::str_fmt("%16lX", meta.titleID), d->GetAllocator());
-		d->AddMember("buildId", Util::str_fmt("%02x%02x%02x%02x%02x%02x%02x%02x", meta.buildID[0], meta.buildID[1], meta.buildID[2], meta.buildID[3], meta.buildID[4], meta.buildID[5], meta.buildID[6], meta.buildID[7]), d->GetAllocator());
+				 d->AddMember("titleId", Util::str_fmt("%16lX", meta.titleID), d->GetAllocator());
+				 d->AddMember("buildId", Util::str_fmt("%02x%02x%02x%02x%02x%02x%02x%02x", meta.buildID[0], meta.buildID[1], meta.buildID[2], meta.buildID[3], meta.buildID[4], meta.buildID[5], meta.buildID[6], meta.buildID[7]), d->GetAllocator());
 
-		setContent(res, Json::toString(d));
-	});
+				 setContent(res, Json::toString(d));
+			 });
 
 	svr->Post("/", [](const httplib::Request &req, httplib::Response &res)
 			  {
-		if (validateAuthentication(req, res) == false)
-			return;
+				  if (validateAuthentication(req, res) == false)
+					  return;
 
-		std::unique_ptr<Document> document = std::make_unique<Document>();
+				  std::unique_ptr<Document> document = std::make_unique<Document>();
 
-		document->Parse(req.body);
+				  document->Parse(req.body);
 
-		if (document->IsObject() == false)
-		{
-			badRequest(res, "Expected Object as Root");
-			return;
-		}
+				  if (document->IsObject() == false)
+				  {
+					  badRequest(res, "Expected Object as Root");
+					  return;
+				  }
 
-		if (document->HasMember("command") == false)
-		{
-			badRequest(res, "Expected command property to be present");
-			return;
-		}
+				  if (document->HasMember("command") == false)
+				  {
+					  badRequest(res, "Expected command property to be present");
+					  return;
+				  }
 
-		if ((*document)["command"].IsString() == false)
-		{
-			badRequest(res, "Expected command property to have type String");
-			return;
-		}
+				  if ((*document)["command"].IsString() == false)
+				  {
+					  badRequest(res, "Expected command property to have type String");
+					  return;
+				  }
 
-		std::string command = (*document)["command"].GetString();
+				  std::string command = (*document)["command"].GetString();
 
-		auto &f = std::use_facet<std::ctype<char>>(std::locale());
-		f.tolower(command.data(), command.data() + command.size());
+				  auto &f = std::use_facet<std::ctype<char>>(std::locale());
+				  f.tolower(command.data(), command.data() + command.size());
 
-		if (command == "press" || command == "click" || command == "release")
-		{
-			if (document->HasMember("button") == false)
-			{
-				badRequest(res, "Expected button property to be present");
-				return;
-			}
+				  if (command == "press" || command == "click" || command == "release")
+				  {
+					  if (document->HasMember("button") == false)
+					  {
+						  badRequest(res, "Expected button property to be present");
+						  return;
+					  }
 
-			if ((*document)["button"].IsString() == false)
-			{
-				badRequest(res, "Expected button property to have type String");
-				return;
-			}
+					  if ((*document)["button"].IsString() == false)
+					  {
+						  badRequest(res, "Expected button property to have type String");
+						  return;
+					  }
 
-			std::string btn = (*document)["button"].GetString();
+					  std::string btn = (*document)["button"].GetString();
 
-			f.toupper(btn.data(), btn.data() + btn.size());
+					  f.toupper(btn.data(), btn.data() + btn.size());
 
-			HidNpadButton key = Util::parseStringToButton((char *)btn.c_str());
+					  HidNpadButton key = Util::parseStringToButton((char *)btn.c_str());
 
-			if (command == "press")
-				Commands::press(key);
-			else if (command == "click")
-				Commands::click(key);
-			else if (command == "release")
-				Commands::release(key);
-		}
-		else if (command == "peek" || command == "poke")
-		{
-			if (document->HasMember("offset") == false)
-			{
-				badRequest(res, "Expected offset property to be present");
-				return;
-			}
+					  if (command == "press")
+						  Commands::press(key);
+					  else if (command == "click")
+						  Commands::click(key);
+					  else if (command == "release")
+						  Commands::release(key);
+				  }
+				  else if (command == "peek" || command == "poke")
+				  {
+					  if (document->HasMember("offset") == false)
+					  {
+						  badRequest(res, "Expected offset property to be present");
+						  return;
+					  }
 
-			if ((*document)["offset"].IsString() == false)
-			{
-				badRequest(res, "Expected offset property to have type String");
-				return;
-			}
+					  if ((*document)["offset"].IsString() == false)
+					  {
+						  badRequest(res, "Expected offset property to have type String");
+						  return;
+					  }
 
-			u8 *val = NULL;
-			u64 size = 0;
+					  std::string mode = "heap";
 
-			if (command == "poke")
-			{
-				if (document->HasMember("value") == false)
-				{
-					badRequest(res, "Expected value property to be present");
-					return;
-				}
+					  if (document->HasMember("mode") == true)
+					  {
+						  mode = (*document)["mode"].GetString();
+						  if (mode != "heap" && mode != "main" && mode != "absolute")
+						  {
+							  badRequest(res, "Mode needs to be either \"heap\", \"main\" or \"absolute\"");
+							  return;
+						  }
+					  }
 
-				if ((*document)["value"].IsString() == false)
-				{
-					badRequest(res, "Expected value property to have type String");
-					return;
-				}
+					  u8 *val = NULL;
+					  u64 size = 0;
 
-				val = Util::parseStringToByteBuffer((char *)(*document)["value"].GetString(), &size);
-			}
-			else
-			{
-				if (document->HasMember("size") == false)
-				{
-					badRequest(res, "Expected size property to be present");
-					return;
-				}
+					  if (command == "poke")
+					  {
+						  if (document->HasMember("value") == false)
+						  {
+							  badRequest(res, "Expected value property to be present");
+							  return;
+						  }
 
-				if ((*document)["size"].IsUint64() == false)
-				{
-					badRequest(res, "Expected size property to have type uint64");
-					return;
-				}
-			}
+						  if ((*document)["value"].IsString() == false)
+						  {
+							  badRequest(res, "Expected value property to have type String");
+							  return;
+						  }
 
-			Commands::MetaData meta = Commands::getMetaData();
-			u64 offset = Util::parseStringToInt((char *)(*document)["offset"].GetString());
+						  val = Util::parseStringToByteBuffer((char *)(*document)["value"].GetString(), &size);
+					  }
+					  else
+					  {
+						  if (document->HasMember("size") == false)
+						  {
+							  badRequest(res, "Expected size property to be present");
+							  return;
+						  }
 
-			if (command == "poke")
-			{
-				Commands::poke(meta.heap_base + offset, size, val);
+						  if ((*document)["size"].IsUint64() == false)
+						  {
+							  badRequest(res, "Expected size property to have type uint64");
+							  return;
+						  }
+					  }
 
-				delete val;
-			}
-			else
-			{
-				size = (*document)["size"].GetUint64();
+					  Commands::MetaData meta = Commands::getMetaData();
+					  u64 offset = Util::parseStringToInt((char *)(*document)["offset"].GetString());
 
-				std::string str = Commands::peekReturn(meta.heap_base + offset, size);
+					  if (command == "poke")
+					  {
+						  if (mode == "heap")
+						  {
+							  Commands::poke(meta.heap_base + offset, size, val);
+						  }
+						  else if (mode == "main")
+						  {
+							  Commands::poke(meta.main_nso_base + offset, size, val);
+						  }
+						  else if (mode == "absolute")
+						  {
+							  Commands::poke(offset, size, val);
+						  }
 
-				std::shared_ptr<Document> d = Json::createDocument();
+						  delete val;
+					  }
+					  else
+					  {
+						  size = (*document)["size"].GetUint64();
 
-				d->AddMember("value", str, d->GetAllocator());
+						  std::string peekRes = "";
 
-				setContent(res, Json::toString(d));
+						  if (mode == "heap")
+						  {
+							  peekRes = Commands::peekReturn(meta.heap_base + offset, size);
+						  }
+						  else if (mode == "main")
+						  {
+							  peekRes = Commands::peekReturn(meta.main_nso_base + offset, size);
+						  }
+						  else if (mode == "absolute")
+						  {
+							  peekRes = Commands::peekReturn(offset, size);
+						  }
 
-				return;
-			}
-		}
-		else
-		{
-			badRequest(res, "Invalid Command");
-			return;
-		}
+						  std::shared_ptr<Document> d = Json::createDocument();
 
-		res.status = 204;
-	});
+						  d->AddMember("value", peekRes, d->GetAllocator());
+
+						  setContent(res, Json::toString(d));
+
+						  return;
+					  }
+				  }
+				  else
+				  {
+					  badRequest(res, "Invalid Command");
+					  return;
+				  }
+
+				  res.status = 204;
+			  });
 }
 
 bool Http::validateAuthentication(const httplib::Request &req, httplib::Response &res)
